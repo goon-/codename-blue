1e789256ad3d90ac6e966be158fd32034e87ec64
goon <garbtr@gmail.com>
2018-06-03 13:46:13 +0700

    adjust player implementation to actors model

--- fm/game_rules.py
+++ fm/game_rules.py
@@ -1,12 +1,12 @@
 import logging
 
+from core.actors.actor_driver import ActorDriver
 from core.drivers.driver import Driver
 from core.gametime import GameTime
 from core.glob import entity_registry, get_vec_fact
 from core.input.devices.keyboard import KEYS, Keyboard
 from core.input.key_mapping import KeyMapping
 from core.input.player_input import PlayerInput
-from core.world import World
 from fm.physics.spacecraft import SpacecraftPhysics
 from fm.player import FmPlayer
 from fm.wall import Wall
@@ -15,7 +15,6 @@ logger = logging.getLogger(__name__)
 
 
 class FmGameRules(Driver):
-    SPEED_CAP = 100.0
     COLLSION_CAT_BACKGROUND = 0
     COLLSION_CAT_PLAYER = 1
     COLLSION_CAT_WALL = 2
@@ -43,23 +42,9 @@ class FmGameRules(Driver):
         physics.set_collidable_categories(self.COLLSION_CAT_BACKGROUND, [])
         physics.set_collidable_categories(self.COLLSION_CAT_PLAYER, [])
         entity_registry.add(physics)
+        entity_registry.add(ActorDriver(20))
         self._game_time = entity_registry.get_by_class(GameTime)[0]
 
     def run(self, skip_frame):
-        if self._player.player_input.is_pressed(0) and self._player.velocity.len() <= self.SPEED_CAP:
-            self._player.force.x += 6000
-
-        if self._player.player_input.is_pressed(1) and self._player.velocity.len() <= self.SPEED_CAP:
-            self._player.force.x -= 6000
-
-        if self._player.player_input.is_pressed(2) and self._player.velocity.len() <= self.SPEED_CAP:
-            self._player.force.y += 6000
-
-        if self._player.player_input.is_pressed(3) and self._player.velocity.len() <= self.SPEED_CAP:
-            self._player.force.y -= 6000
-
-        if self._player.player_input.is_pressed(4):
-            logger.info('Exiting')
-            entity_registry.get_by_class(World)[0].stop()
 
         return True
--- fm/player.py
+++ fm/player.py
@@ -1,10 +1,17 @@
-from core.glob import get_vec_fact
+import logging
+
+from core.glob import get_vec_fact, entity_registry
 from core.physics.entities.dynamic_physic_entity import DynamicPhysicEntity
 from core.player import Player
+from core.world import World
 from pygame_impl.graphics.entites.placeholder import Placeholder
+
+logger = logging.getLogger(__name__)
 
 
 class FmPlayer(Player, Placeholder, DynamicPhysicEntity):
+    SPEED_CAP = 100.0
+
     def __init__(self, player_input, position, z=0, collision_category=None):
         size = get_vec_fact().vector2(20, 10)
         super(FmPlayer, self).__init__(
@@ -16,3 +23,20 @@ class FmPlayer(Player, Placeholder, DynamicPhysicEntity):
             z=z,
             collision_category=collision_category
         )
+
+    def think(self):
+        if self.player_input.is_pressed(0) and self.velocity.len() <= self.SPEED_CAP:
+            self.force.x += 6000
+
+        if self.player_input.is_pressed(1) and self.velocity.len() <= self.SPEED_CAP:
+            self.force.x -= 6000
+
+        if self.player_input.is_pressed(2) and self.velocity.len() <= self.SPEED_CAP:
+            self.force.y += 6000
+
+        if self.player_input.is_pressed(3) and self.velocity.len() <= self.SPEED_CAP:
+            self.force.y -= 6000
+
+        if self.player_input.is_pressed(4):
+            logger.info('Exiting')
+            entity_registry.get_by_class(World)[0].stop()
