f78b1a41356d57899e94f82a9fa51813bba69ca7
goon <goon@goon.goon>
2018-10-21 18:37:09 +0700

    aim projectiles with mouse

--- fm/fm_player.py
+++ fm/fm_player.py
@@ -54,13 +54,26 @@ class FmPlayer(Player, Placeholder, DynamicPhysicEntity):
             entity_registry.get_by_class(World)[0].stop()
 
         if self.player_input.is_pressed(self.ACT_FIRE):
-            if self._game_time.now - self._last_shot_time >= self.fire_cooldown:
-                entity_registry.add(Projectile(
-                    self.position + get_vec_fact().vector2(21, 0), get_vec_fact().vector2(1000, 0), (0, 0, 255),
-                    velocity=get_vec_fact().vector2(300, 0), mass=1, z=self.z,
-                    collision_category=self.projectile_collision_category
-                ))
-                self._last_shot_time = self._game_time.now
+            self.shoot()
+
+    def shoot(self):
+        if self._game_time.now - self._last_shot_time < self.fire_cooldown:
+            return
+
+        projectile_spawn_position = self.position + get_vec_fact().vector2(21, 0)
+        projectile_direction = self._calc_normalized_projectile_direction(projectile_spawn_position)
+        entity_registry.add(Projectile(
+            projectile_spawn_position, projectile_direction * 1000, (0, 0, 255),
+            velocity=projectile_direction * 300, mass=1, z=self.z,
+            collision_category=self.projectile_collision_category
+        ))
+        self._last_shot_time = self._game_time.now
+
+    def _calc_normalized_projectile_direction(self, spawn_position):
+        mouse_position = self.player_input.get_mouse_world_pos()
+        direction = get_vec_fact().vector2(*mouse_position) - spawn_position
+        direction.normalize()
+        return direction
 
     @property
     def time_until_next_shot(self):
